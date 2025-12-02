e504d96399afd43df922227d942f2b873525a235
lulwakamel <lulwakamel@gmail.com>
2025-12-02 19:56:16 +0300

    Fix connection error handling in db.php

--- src/discussion/db.php
+++ src/discussion/db.php
@@ -12,7 +12,7 @@ class Database {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $exception) {
-            echo "Connection error: " . $exception->getMessage();
+            die "Connection error: " . $exception->getMessage();
         }
         return $this->conn;
     }
